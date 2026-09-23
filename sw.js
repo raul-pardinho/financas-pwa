080632f31dba334d5fa044a1080d81fe842ee709
raul-pardinho <raulpardinho@gmail.com>
2026-09-22 22:55:27 -0300

    Adiciona sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='financas-v1.0.1';
+const LOCAL=['./','index.html','styles.css','app.js','manifest.webmanifest','assets/icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return resp;}).catch(()=>caches.match('./index.html'))));});
